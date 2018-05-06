ac070c92c0641c1ee5ac6def7fc39b6de96a45e3
DESKTOP-NCMIQHK\Hernan <hernanpocchiola@gmail.com>
2018-05-06 13:06:20 -0300

    Cambios en el login fb

--- public/javascripts/loginFB.js
+++ public/javascripts/loginFB.js
@@ -7,8 +7,8 @@
 		    });
 		    FB.getLoginStatus(function(response) {
 		    	if (response.status === 'connected') {
-		    		document.getElementById('status').innerHTML = 'We are connected.';
-		    		document.getElementById('login').style.visibility = 'hidden';
+		    		//document.getElementById('status').innerHTML = 'We are connected.';
+		    		//document.getElementById('login').style.visibility = 'hidden';
 		    	} else if (response.status === 'not_authorized') {
 		    		document.getElementById('status').innerHTML = 'We are not logged in.'
 		    	} else {
