d9456b98ef1700ad2252c0a99e04b8875e1a7a6e
DESKTOP-NCMIQHK\Hernan <hernanpocchiola@gmail.com>
2018-05-08 13:22:03 -0300

    Heroku andando con equipos favs

--- app_server/controllers/auth.js
+++ app_server/controllers/auth.js
@@ -7,8 +7,8 @@ var User = require('../models/users');
 passport.use(new Strategy({
     clientID: '168126427354074',
     clientSecret: '53335c45156e66fa96d9e650edf7d6c7', 
- //   callbackURL: "https://hstournament.herokuapp.com/auth/facebook/callback"
-    callbackURL: "https://8e33f2df.ngrok.io/auth/facebook/callback"
+    callbackURL: "https://hstournament.herokuapp.com/auth/facebook/callback"
+    //callbackURL: "https://8e33f2df.ngrok.io/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     //check user table for anyone with a facebook ID of profile.id
